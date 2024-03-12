a576563c9f04b5046937be9a8b48083deb9f02de
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-12 18:50:03 +0530

    stop running instances script

---
+++ disableInstancesScript.py
@@ -0,0 +1,15 @@
+import boto3
+
+def disable_instances(instance_ids):
+  ec2_client = boto3.client("ec2")
+  response = ec2_client.stop_instances(InstanceIds=instance_ids)
+  return response
+
+def main():
+  instance_ids = ['i-06f0a0001f4439698','i-061caf661fc241a18','i-033664e5a2570d1f7']
+  response = disable_instances(instance_ids)
+  print(response)
+  
+
+if __name__ == "__main__":
+  main()
