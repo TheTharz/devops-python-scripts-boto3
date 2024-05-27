d4008bdf2691124fed51d4408426d6b01a5053db
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-05-27 23:03:14 +0530

    Update describeInstances.py

--- describeInstances.py
+++ describeInstances.py
@@ -1,8 +1,21 @@
 import boto3
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances()
-instances = response['Reservations'][0]['Instances']
-print(instances)
-for instance in instances:
-  print(instance['InstanceId'])
 
+# Create a session to interact with AWS services
+session = boto3.Session()
+
+# Get all available regions
+regions = session.get_available_regions('ec2')
+
+# Iterate over each region
+for region in regions:
+    print(f"Instances in region: {region}")
+
+    # Create an EC2 client for the current region
+    ec2 = session.client('ec2', region_name=region)
+
+    # Describe instances in the current region
+    response = ec2.describe_instances()
+
+    # Extract instance information and print
+    instances = [instance['InstanceId'] for reservation in response['Reservations'] for instance in reservation['Instances']]
+    print(instances)
