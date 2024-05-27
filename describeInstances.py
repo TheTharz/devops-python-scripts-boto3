344c0bd8d33bb849ca868fbaee29aca0bf23a6e3
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-05-27 23:01:22 +0530

    Update describeInstances.py

--- describeInstances.py
+++ describeInstances.py
@@ -2,7 +2,7 @@ import boto3
 ec2 = boto3.client('ec2')
 response = ec2.describe_instances()
 instances = response['Reservations'][0]['Instances']
-
+print(instances)
 for instance in instances:
   print(instance['InstanceId'])
 
