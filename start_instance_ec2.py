f60f717c274a79adca8fe1811627b1faef74f76b
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-06-13 14:48:19 +0530

    Update start_instance_ec2.py

--- start_instance_ec2.py
+++ start_instance_ec2.py
@@ -12,6 +12,10 @@ def start_ec2_instance(instance_name):
         }
     ])
 
+    if not response['Reservations']:
+        print(f"No instances found with the name '{instance_name}'.")
+        return
+
     # Extract the instance ID from the response
     instance_id = response['Reservations'][0]['Instances'][0]['InstanceId']
 
@@ -19,6 +23,16 @@ def start_ec2_instance(instance_name):
     ec2_client.start_instances(InstanceIds=[instance_id])
     print(f"Instance with name '{instance_name}' (ID: {instance_id}) is starting.")
 
-# Replace 'your-instance-name' with the name of your EC2 instance
-instance_name = 'Designflow'
+    # Wait for the instance to be in a 'running' state
+    waiter = ec2_client.get_waiter('instance_running')
+    waiter.wait(InstanceIds=[instance_id])
+
+    # Get the public IP address of the instance
+    instance_info = ec2_client.describe_instances(InstanceIds=[instance_id])
+    public_ip = instance_info['Reservations'][0]['Instances'][0]['PublicIpAddress']
+
+    print(f"The public IP address of the instance '{instance_name}' is: {public_ip}")
+
+# Prompt user for the instance name
+instance_name = input("Enter the name of the EC2 instance: ")
 start_ec2_instance(instance_name)
