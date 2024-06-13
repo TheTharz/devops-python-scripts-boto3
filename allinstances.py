de21dcec4935870f1e71d3ee651e6063a342d7cb
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-06-13 14:52:12 +0530

    Update allinstances.py

--- allinstances.py
+++ allinstances.py
@@ -22,6 +22,13 @@ for reservation in response['Reservations']:
         instance_state = instance['State']['Name']
         instance_public_ip = instance.get('PublicIpAddress', 'N/A')
         instance_private_ip = instance.get('PrivateIpAddress', 'N/A')
+
+        # Get the name tag of the instance
+        instance_name = 'Unnamed'
+        for tag in instance.get('Tags', []):
+            if tag['Key'] == 'Name':
+                instance_name = tag['Value']
+                break
         
         # Append instance details to the list
         instances.append({
@@ -29,11 +36,13 @@ for reservation in response['Reservations']:
             'InstanceType': instance_type,
             'InstanceState': instance_state,
             'PublicIP': instance_public_ip,
-            'PrivateIP': instance_private_ip
+            'PrivateIP': instance_private_ip,
+            'Name': instance_name
         })
 
 # Print the list of running instances
 for instance in instances:
+    print(f"Instance Name: {instance['Name']}")
     print(f"Instance ID: {instance['InstanceID']}")
     print(f"Instance Type: {instance['InstanceType']}")
     print(f"Instance State: {instance['InstanceState']}")
